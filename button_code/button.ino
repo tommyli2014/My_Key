701c76df46355412814c581db4fc7d2112947e58
tommyli2014 <tommyli76278@gmail.com>
2015-07-21 14:03:59 -0700

    button login code

---
+++ button_code/button.ino
@@ -0,0 +1,61 @@
+//#include <Keyboard.h>
+
+
+/*
+  Button
+
+ Turns on and off a light emitting diode(LED) connected to digital
+ pin 13, when pressing a pushbutton attached to pin 2.
+
+
+ The circuit:
+ * LED attached from pin 13 to ground
+ * pushbutton attached to pin 4 from +5V
+ * 10K resistor attached to pin 2 from ground
+
+ * Note: on most Arduinos there is already an LED on the board
+ attached to pin 13.
+
+
+ created 2005
+ by DojoDave <http://www.0j0.org>
+ modified 30 Aug 2011
+ by Tom Igoe
+
+ This example code is in the public domain.
+
+ http://www.arduino.cc/en/Tutorial/Button
+ */
+
+// constants won't change. They're used here to
+// set pin numbers:
+const int buttonPin = 4;     // the number of the pushbutton pin     // the number of the LED pin
+
+// variables will change:
+int buttonState = 0;         // variable for reading the pushbutton status
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  Keyboard.begin();
+}
+
+void loop() {
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed.
+  if (buttonState == HIGH) {
+ 
+  Keyboard.print("type your username here");
+  Keyboard.press(KEY_TAB);
+  delay(100);
+  Keyboard.print("type your password here");
+  Keyboard.press(KEY_RETURN);
+    delay(100);
+    Keyboard.releaseAll();
+    
+   
+  }
+ 
+}
+
